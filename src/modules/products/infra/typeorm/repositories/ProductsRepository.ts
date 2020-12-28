019c81da028aed957d701d37cb97573119828a7c
Pedro Sanção <pedrosancao@users.noreply.github.com>
2020-12-27 23:21:11 -0300

    Create new product route

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -8,6 +8,9 @@ import Product from '../entities/Product';
 interface IFindProducts {
   id: string;
 }
+interface IProductsQuantityMap {
+  [key: string]: number;
+}
 
 class ProductsRepository implements IProductsRepository {
   private ormRepository: Repository<Product>;
@@ -21,21 +24,47 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({ name, price, quantity });
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const product = await this.ormRepository.findOne({
+      where: { name },
+    });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const foundProducts = await this.ormRepository.find({
+      where: {
+        id: In(products.map(product => product.id)),
+      },
+    });
+    return foundProducts;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const quantityMap: IProductsQuantityMap = {};
+    products.forEach(({ id, quantity }) => {
+      quantityMap[id] = quantity;
+    });
+    const foundProducts = await this.findAllById(products);
+    const updatedProducts = foundProducts.map(({ id, ...properties }) => {
+      return {
+        id,
+        ...properties,
+        quantity: quantityMap[id],
+      };
+    });
+    await this.ormRepository.save(updatedProducts);
+
+    return updatedProducts;
   }
 }
 
---
+++ src/shared/infra/typeorm/migrations/1609118840911-CreateProductsTable.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateProductsTable1609118840911
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 9,
+            scale: 2,
+          },
+          {
+            name: 'quantity',
+            type: 'integer',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropTable('products');
+  }
+}
